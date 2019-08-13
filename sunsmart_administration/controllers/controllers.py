af478565c511362a91bbc7c94e0513d6b9d7a916
Daven <dsmauff@gmail.com>
2019-08-13 19:41:37 +0200

    fml

--- sunsmart_administration/controllers/controllers.py
+++ sunsmart_administration/controllers/controllers.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class SunsmartAdministration(http.Controller):
-#     @http.route('/sunsmart_administration/sunsmart_administration/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
 
-#     @http.route('/sunsmart_administration/sunsmart_administration/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('sunsmart_administration.listing', {
-#             'root': '/sunsmart_administration/sunsmart_administration',
-#             'objects': http.request.env['sunsmart_administration.sunsmart_administration'].search([]),
-#         })
-
-#     @http.route('/sunsmart_administration/sunsmart_administration/objects/<model("sunsmart_administration.sunsmart_administration"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('sunsmart_administration.object', {
-#             'object': obj
-#         })
+class StudentApplication(http.Controller):
+    @http.route('/application', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
